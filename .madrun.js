f013e6316d125629b103d7631d35571511799538
coderiaser <coderaiser@cloudcmd.io>
2025-01-17 16:56:29 +0200

    chore: lint

--- .madrun.js
+++ .madrun.js
@@ -12,6 +12,7 @@ module.exports = {
     'fix:lint': () => run('lint', '--fix'),
     'coverage': async () => `c8 ${await run('test')}`,
     'patch': () => 'vsce publish patch --allow-star-activation',
+    'minor': () => 'vsce publish minor --allow-star-activation',
     'report': () => 'c8 report --reporter=lcov',
     'package': () => 'vsce package --allow-star-activation',
     'upload': () => `putasset --filename vscode-putout-${readVersion()}.vsix --tag v${readVersion()} --r vscode-putout -o putoutjs`,
